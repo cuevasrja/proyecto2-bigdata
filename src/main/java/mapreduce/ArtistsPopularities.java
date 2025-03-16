4198ba6183572dc2c73e49f7864e681a68cc551c
Juan Cuevas <19-10056@usb.ve>
2025-03-16 19:22:37 -0400

    Modificar ArtistsPopularitiesMapper y ArtistsPopularitiesReducer para utilizar FloatWritable en lugar de IntWritable, mejorando la precisión en el manejo de la popularidad de las canciones.

--- src/main/java/mapreduce/ArtistsPopularities.java
+++ src/main/java/mapreduce/ArtistsPopularities.java
@@ -15,10 +15,9 @@ import spotify.Track;
 public class ArtistsPopularities extends Configured implements Tool {
     static SpotifyCsvParser parser = new SpotifyCsvParser();
 
-    public static class ArtistsPopularitiesMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable[]> {
+    public static class ArtistsPopularitiesMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, FloatWritable> {
         private Text artist = new Text();
-        private IntWritable songPopularity = new IntWritable();
-        private IntWritable artistPopularity = new IntWritable();
+        private FloatWritable songPopularity = new FloatWritable();
 
         /**
          * Map the input values.
@@ -28,17 +27,20 @@ public class ArtistsPopularities extends Configured implements Tool {
          * @param reporter Facility to report progress.
          * @throws IOException
          */
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable[]> output, Reporter reporter) throws IOException {
+        public void map(LongWritable key, Text value, OutputCollector<Text, FloatWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
+            if (line.startsWith("\"id\"")) {
+                return;
+            }
+
             Track track = parser.parseCSVLine(line);
             artist.set(track.get("artist_name"));
-            songPopularity.set(Integer.parseInt(track.get("popularity")));
-            artistPopularity.set(Integer.parseInt(track.get("artist_popularity")));
-            output.collect(artist, new IntWritable[]{songPopularity, artistPopularity});
+            songPopularity.set(Float.parseFloat(track.get("popularity")));
+            output.collect(artist, songPopularity);
         }
     }
-    
-    public static class ArtistsPopularitiesReducer extends MapReduceBase implements Reducer<Text, IntWritable[], Text, FloatWritable> {
+
+    public static class ArtistsPopularitiesReducer extends MapReduceBase implements Reducer<Text, FloatWritable, Text, FloatWritable> {
         /**
          * Reduce the values for each key.
          * @param key The key.
@@ -47,19 +49,15 @@ public class ArtistsPopularities extends Configured implements Tool {
          * @param reporter Facility to report progress.
          * @throws IOException
          */
-        public void reduce(Text key, Iterator<IntWritable[]> values, OutputCollector<Text, FloatWritable> output, Reporter reporter) throws IOException {
+        public void reduce(Text key, Iterator<FloatWritable> values, OutputCollector<Text, FloatWritable> output, Reporter reporter) throws IOException {
             float sum = 0;
-            IntWritable[] actual = values.next();
-            int artistPopularity = actual[1].get();
-            int count = 1;
-            do {
-                sum += actual[0].get();
-                actual = values.next();
+            int count = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
                 count++;
-            } while (values.hasNext());
-            sum /= count;
-            float diff = sum - artistPopularity;
-            output.collect(key, new FloatWritable(diff));
+            }
+            float avg = sum / count;
+            output.collect(key, new FloatWritable(avg));
         }
     }
 
@@ -92,7 +90,7 @@ public class ArtistsPopularities extends Configured implements Tool {
         JobClient.runJob(conf);
         return 0;
     }
-    
+
     public void execute(String[] args) throws Exception {
         int res = ToolRunner.run(new ArtistsPopularities(), args);
         System.exit(res);
